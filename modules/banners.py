561ad194f20c80798369d2e52bf5c0e28f65297a
Pari Malam <apid9755@gmail.com>
2025-08-17 01:21:19 +0800

    + release v0.2

--- modules/banners.py
+++ modules/banners.py
@@ -24,4 +24,4 @@ def banners():
     stdout.write(""+Fore.YELLOW   +"╔════════════════════════════════════════════════════════════════════════════╝\n")
     stdout.write(""+Fore.YELLOW   +"║ \x1b[38;2;255;20;147m• "+Fore.GREEN+"GITHUB             "+Fore.RED+"    |"+Fore.LIGHTWHITE_EX+"   GITHUB.COM/THATNOTEASY                        "+Fore.YELLOW+"║\n")
     stdout.write(""+Fore.YELLOW   +"╚════════════════════════════════════════════════════════════════════════════╝\n") 
-    print(f"{Fore.YELLOW}[ReCrypt] - {Fore.GREEN}A comprehensive security suite specializing in PlayReady - {Fore.RED}[V0.3]{Fore.RESET}\n")
+    print(f"{Fore.YELLOW}[ReCrypt] - {Fore.GREEN}A comprehensive security suite specializing in PlayReady - {Fore.RED}[V0.2]{Fore.RESET}\n")
