96e449be47e9faba6dbab126548dcf545ed8effb
Pari Malam <apid9755@gmail.com>
2025-08-16 11:36:15 +0800

    + add banners

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from modules.zgpriv import ZGPRIV
-import os, time
+from modules.banners import banners
+import os
 
 def main():
     zgpriv = ZGPRIV()
     zgpriv.analyzer_zgpriv()
     
 if __name__ == "__main__":
-    os.system('cls' if os.name == 'nt' else 'clear')
+    banners()
     main()
